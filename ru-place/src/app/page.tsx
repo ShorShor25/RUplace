1dc45eecf8552e7d57e420443db9e25adbe7dbe2
Alex Varjabedian <alex.varjabedian2026@gmail.com>
2025-10-04 17:22:44 -0400

    socket complete

--- ru-place/src/app/page.tsx
+++ ru-place/src/app/page.tsx
@@ -1,23 +1,22 @@
 'use client';
 
 import Map from '@/components/map';
-import { initSocket, sendTileUpdate } from '../../websocket/wsocket';
+import { socket, initSocket, sendTileUpdate } from '../../websocket/wsocket';
 import { Tile } from '../../shared/tile'
 import { useEffect } from 'react'
 
 export default function Home() {
   useEffect(() => {
-    initSocket();
-    const tile: Tile = { "x": 1, "y": 2, "color": 6, "lat": 69.69, "long": 47.74 };
-    sendTileUpdate(tile);
-    sendTileUpdate(tile);
-    sendTileUpdate(tile);
-    sendTileUpdate(tile);
+    const run = async () => {
+      const tile: Tile = { "x": 1, "y": 2, "color": 6, "lat": 69.69, "long": 47.74 };
+      sendTileUpdate(tile);
+    }
+    run();
   }, []);
 
   return (
     <main style={{ height: '100vh', width: '100%', position: 'relative' }}>
-      <Map/>
+      <Map />
     </main>
   );
 }
