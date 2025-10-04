4346f8db61e16b51174d4b039c39b55d58422d7f
Alex Varjabedian <alex.varjabedian2026@gmail.com>
2025-10-04 16:02:54 -0400

    websocket progress

--- ru-place/src/app/page.tsx
+++ ru-place/src/app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 
 import Map from '@/components/map'
-import { connect } from '../../websocket/wsocket'
+import { initSocket, sendTileUpdate } from '../../websocket/wsocket'
+import { Tile } from '../../shared/tile'
+import { useEffect } from 'react'
 
 export default function Home() {
-  connect();
+  useEffect(() => {
+    initSocket();
+    const tile: Tile = { "x": 1, "y": 2, "color": 6, "lat": 69.69, "long": 47.74 };
+    sendTileUpdate(tile);
+  }, []);
+
   return (
     <main style={{ height: '100vh', width: '100%' }}>
       <Map />
