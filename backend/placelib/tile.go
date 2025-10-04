7e30ed4ae180a8785c41164b4a642ab0d929fa45
Ramesh Balaji <me@ramesh.tv>
2025-10-04 13:39:42 -0400

    backend: timer

--- backend/placelib/tile.go
+++ backend/placelib/tile.go
@@ -1,1 +1,16 @@
 package placelib
+
+import (
+	"log"
+	"time"
+)
+
+// Update the tile database in a timer loop
+func UpdateTileDbLoop() {
+	dbUpdateTimer := time.NewTimer(500 * time.Millisecond)
+
+	for {
+		<-dbUpdateTimer.C
+		log.Print("tile db update fired")
+	}
+}
