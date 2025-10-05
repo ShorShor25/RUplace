51df43cbc0986d13c19f3f9139beee446ef34e0d
Alex Varjabedian <alex.varjabedian2026@gmail.com>
2025-10-05 11:33:04 -0400

    cleanup

--- ru-place/api/wsocket.tsx
+++ ru-place/api/wsocket.tsx
@@ -4,25 +4,13 @@ import { Tile } from "../shared/tile";
 export let socket: WebSocket | null = null;
 
 export function initSocket() {
-        if (socket != null && socket.readyState === WebSocket.OPEN) {
-            return;
-        }
-
-        socket = new WebSocket("ws://127.0.0.1:8080/ws");
-
-        /*socket.addEventListener("open", (event) => {
-            resolve();
-        });
-
-
-        socket.addEventListener("error", (event) => {
-            console.error("WebSocket error observed: ", event);
-            reject(event);
-        });*/
+    if (socket != null && socket.readyState === WebSocket.OPEN) {
+        return;
+    }
+    socket = new WebSocket("ws://127.0.0.1:8080/ws");
 }
 
 export async function sendTileUpdate(tile: Tile) {
-    console.log("tile update")
     const rpc = { rpcName: "clientTileUpdate", payload: tile };
     socket!.send(JSON.stringify(rpc));
 }
